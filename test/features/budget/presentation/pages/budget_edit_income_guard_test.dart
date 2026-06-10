12b2c48f6a41ee966d2316331f83794c778265fb
devanoahmadd <devanoahmadd@gmail.com>
2026-06-11 01:44:36 +0700

    test(budget): hapus unused mocktail import di income guard test

--- test/features/budget/presentation/pages/budget_edit_income_guard_test.dart
+++ test/features/budget/presentation/pages/budget_edit_income_guard_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:penyintas_app/features/budget/presentation/bloc/budget_settings_bloc.dart';
 import 'package:penyintas_app/features/budget/presentation/pages/budget_edit_settings_page.dart';
 
