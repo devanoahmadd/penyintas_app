8c72ae9d5831b7c777643753c38c3ed4d2b6481c
devanoahmadd <devanoahmadd@gmail.com>
2026-07-01 19:25:44 +0700

    fix(auth): hapus unnecessary import flutter/foundation.dart di splash_page

--- lib/features/auth/presentation/pages/splash_page.dart
+++ lib/features/auth/presentation/pages/splash_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
