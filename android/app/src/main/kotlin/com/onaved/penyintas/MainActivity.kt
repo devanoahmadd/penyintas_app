685b5a1228e66b56e6574020e5940970cb049dd7
devanoahmadd <devanoahmadd@gmail.com>
2026-07-15 16:30:10 +0700

    chore(app-lock): tambah dependency secure_storage/local_auth/crypto + platform config

--- android/app/src/main/kotlin/com/onaved/penyintas/MainActivity.kt
+++ android/app/src/main/kotlin/com/onaved/penyintas/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.onaved.penyintas
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
